3fae9862edc34b7c349e2e1a37cfdf620a8ce513
Tarun77461 <tarun@triosoft.ai>
2025-11-28 13:26:22 +0530

    Update

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
 const mysql = require("mysql2");
-
-// Just for render/git detection 👇 (safe change)
-console.log("🔧 Loading MySQL Connection Using ENV Variables...");
 
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
